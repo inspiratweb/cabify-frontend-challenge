489fcf129c9d7ba56824c1969685bce5d72ee3fa
Francisco Sanchez <francisco.sanchez@cabify.com>
2018-07-05 14:14:09 +0200

    businessCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import cabifyLogo from './images/cabify-logo.svg';
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="mainWrapper row">
+        <article className="businessCard col col6">
+          <figure className="businessCard-badge">
+            <a className="businessCard-badge-logo" href="http://www.cabify.com">
+              <img src={cabifyLogo} alt="Cabify" />
+            </a>
+          </figure>
+          <h1 className="title-main">Request your business card</h1>
+          <div className="businessCard-cards">
+            <div className="businessCard-cardBack" />
+            <div className="businessCard-cardFront">
+            </div>
+          </div>
+        </article>
+        <form className="builder" action="">
+
+        </form>
       </div>
     );
   }
