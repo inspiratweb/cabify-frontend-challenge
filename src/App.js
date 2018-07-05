2f1022b16f1b7ff25474fb0be210a62f35462d81
Francisco Sanchez <francisco.sanchez@cabify.com>
2018-07-05 15:23:18 +0200

    form

--- src/App.js
+++ src/App.js
@@ -19,9 +19,65 @@ class App extends Component {
             </div>
           </div>
         </article>
-        <form className="builder" action="">
-
-        </form>
+        <article className="builder col col6">
+          <form className="form" action="">
+            <div className="row">
+              <div class="formField-input active focus col col12">
+                <div class="input">
+                  <input type="text" name="fullname" value="Laura Sánchez" />
+                  <label for="fullname">Full name</label>
+                </div>
+              </div>
+            </div>
+            <div className="row row-separationMedium">
+              {/* you probably need to add active/focus/disabled classNames */}
+              <div class="formField-input col col12">
+                <div class="input">
+                  <input type="text" name="jobdescription" />
+                  <label for="jobdescription">Job description</label>
+                </div>
+              </div>
+            </div>
+            <div className="row row-separationMedium row-gutterMedium">
+              <div class="col col3">
+                {/* select field will be placed here */}
+              </div>
+              <div class="formField-input col col9">
+                <div class="input">
+                  <input type="tel" name="ponenumber" />
+                  <label for="ponenumber">Phone number</label>
+                </div>
+              </div>
+            </div>
+            <div className="row row-separationMedium">
+              <div class="formField-input col col12">
+                <div class="input">
+                  <input type="email" name="email" />
+                  <label for="email">Email</label>
+                </div>
+              </div>
+            </div>
+            <div className="row row-separationMedium">
+              <div class="formField-input active disabled col col12">
+                <div class="input">
+                  <input type="text" name="website" value="www.cabify.com" />
+                  <label for="website">Website</label>
+                </div>
+              </div>
+            </div>
+            <div className="row row-separationMedium">
+              <div class="formField-input active col col12">
+                <div class="input">
+                  <input type="text" name="address" value="Calle Pradillo 42. CP: 28002. Madrid" />
+                  <label for="address">Address</label>
+                </div>
+              </div>
+            </div>
+            <div className="row row-separationHuge">
+              <input className="button button-full button-primary disabled" type="submit" value="Request" />
+            </div>
+          </form>
+        </article>
       </div>
     );
   }
